2d9618cfae10c454acdf1085e04bf7d35fc59f80
sergeihodarkovski <sergeihodarkovski@gmail.com>
2024-11-22 16:08:46 +0100

    Save changes

--- src/server.js
+++ src/server.js
@@ -30,6 +30,7 @@ export const startServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
