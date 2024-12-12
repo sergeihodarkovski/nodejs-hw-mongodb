186a6228be6c605a2af730fc3266b19830cfa44f
sergeihodarkovski <sergeihodarkovski@gmail.com>
2024-12-12 17:02:54 +0100

    Save changes

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -3,5 +3,5 @@ import Joi from 'joi';
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(8).required(),
 });
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,19 +1,20 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20),
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().required(),
   email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16),
-  gender: Joi.string().valid('male', 'female', 'other'),
-  avgMark: Joi.number().min(2).max(12),
-  onDuty: Joi.boolean(),
+  isFavourite: Joi.boolean().default(false),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .default('personal')
+    .required(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string(),
   email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16),
-  gender: Joi.string().valid('male', 'female', 'other'),
-  avgMark: Joi.number().min(2).max(12),
-  onDuty: Joi.boolean(),
-});
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+}).min(1);
